1e1c4ef43204f5389331121f549b3520ed7c4dd8
cole-g-123 <24316cg@hvhs.school.nz>
2026-04-29 02:34:44 +0000

    Apr 29, 2026, 2:34 AM

--- t02_variables.js
+++ t02_variables.js
@@ -7,12 +7,12 @@ console.log( "Running t01_introduction.js" );
 let username = "Cole";
 let year = 2026;
 let age = 15;
-let money = 10;
+let money = 378;
 /****************************
   Main Code
 ****************************/ 
 
-console.log("hi" + username + " as of " + year + "years old. you have" + money + "dollars")
+console.log("Hi " + username + ", as of " + year + " you are " + age + " years old. " + "Congrats, you have $" + money +  "! " + " Unfortunately, Ayush is a broke scammer.")
 
 
 /****************************
