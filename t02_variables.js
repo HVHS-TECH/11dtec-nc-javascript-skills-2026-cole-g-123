feeeba89517b434264f07642694ba59e73ff0cf1
cole-g-123 <24316cg@hvhs.school.nz>
2026-04-29 02:59:06 +0000

    Apr 29, 2026, 2:59 AM

--- t02_variables.js
+++ t02_variables.js
@@ -18,10 +18,8 @@ halfmoney = money / 2
 ****************************/ 
 
 console.log("Hi " + username + ", as of " + year + " you are " + age + " years old. " + "Congrats, you have $" + money +  "!" + " Unfortunately, Ayush is bankrupt as a result of spending too much money on cricket gear.")
-console.log("You were born in " + birthyear )
-console.log("In 10 years you will be " + oldage)
-console.log("You have $" + money)
-console.log("Uh-oh! You spent half of your money! You now have $" + halfmoney)
+
+
 
 /****************************
  Functions
