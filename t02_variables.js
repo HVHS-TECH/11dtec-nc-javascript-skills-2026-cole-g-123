676a43e1aa0760ba1df3603c947b559a1beb9fca
cole-g-123 <24316cg@hvhs.school.nz>
2026-04-29 02:52:57 +0000

    Apr 29, 2026, 2:52 AM

--- t02_variables.js
+++ t02_variables.js
@@ -19,6 +19,7 @@ birthyear = year - age;
 console.log("Hi " + username + ", as of " + year + " you are " + age + " years old. " + "Congrats, you have $" + money +  "!" + " Unfortunately, Ayush is bankrupt as a result of spending too much money on cricket gear.")
 console.log("You were born in " + birthyear )
 console.log("In 10 years you will be " + oldage)
+console.log("You have $" + money)
 
 /****************************
  Functions
