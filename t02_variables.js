4c2fcf268e63187cabb80e50ca14cf928d265f36
cole-g-123 <24316cg@hvhs.school.nz>
2026-04-29 02:48:38 +0000

    Apr 29, 2026, 2:48 AM

--- t02_variables.js
+++ t02_variables.js
@@ -8,12 +8,16 @@ let username = "Cole";
 let year = 2026;
 let age = 15;
 let money = 378;
+let birthyear;
+let oldage;
+oldage = age + 10;
+birthyear = year - age;
 /****************************
   Main Code
 ****************************/ 
 
 console.log("Hi " + username + ", as of " + year + " you are " + age + " years old. " + "Congrats, you have $" + money +  "!" + " Unfortunately, Ayush is bankrupt as a result of spending too much money on cricket gear.")
-
+console.log("You were born in " + birthyear )
 
 /****************************
  Functions
