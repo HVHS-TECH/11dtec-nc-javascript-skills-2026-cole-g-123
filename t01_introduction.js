1d7c3b0c719b467b1639430250496d814a1133d5
cole-g-123 <24316cg@hvhs.school.nz>
2026-04-29 02:19:40 +0000

    Apr 29, 2026, 2:19 AM

--- t01_introduction.js
+++ t01_introduction.js
@@ -1,9 +1,18 @@
 /****************************
-This is a block comment.
-Put your header comment here!
+Name of Task:
 ****************************/
-console.log("Running t01_introduction.js")
-console.log(/*Insert your message here!*/)
+console.log( "Running t01_introduction.js" );
 
-// This is a single line comment
+//Variables
 
+
+/****************************
+  Main Code
+****************************/ 
+
+
+
+
+/****************************
+ Functions
+****************************/ 
--- t02_variables.js
+++ t02_variables.js
@@ -1,1 +1,20 @@
-/* Paste your code from task 1 here */
+/****************************
+Name of Task:
+****************************/
+console.log( "Running t01_introduction.js" );
+
+//Variables
+let username = "Cole";
+let year = 2026;
+let age = 15;
+let money = 10;
+/****************************
+  Main Code
+****************************/ 
+
+console.log("hi"+ username + "as of")
+
+
+/****************************
+ Functions
+****************************/ 
