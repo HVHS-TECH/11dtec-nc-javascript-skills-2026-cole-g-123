35e645595c8933c8dade8e8ab446fa25627fd953
cole-g-123 <24316cg@hvhs.school.nz>
2026-05-03 22:25:20 +0000

    May 3, 2026, 10:25 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -1,1 +1,18 @@
-/* Paste your code from task 3, Maths here */
+
+console.log( "Running t01_introduction.js" );
+
+//Variables
+let username = "Cole";
+let year = 2026;
+let age = 15;
+let money = 378;
+let birthyear;
+let oldage;
+oldage = age + 10;
+birthyear = year - age;
+halfmoney = money / 2
+
+console.log("You were born in " + birthyear )
+console.log("In 10 years you will be " + oldage)
+console.log("You have $" + money)
+console.log("Uh-oh! You spent half of your money! You now have $" + halfmoney)
