00db4fb786c1c7c6da72b0eda585df817ff5e764
cole-g-123 <24316cg@hvhs.school.nz>
2026-05-03 22:59:45 +0000

    May 3, 2026, 10:59 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -8,15 +8,15 @@ let age = 15;
 let money = 378;
 let birthyear;
 let oldage;
-oldage = age + 10;
+oldAge = age + 10;
 birthyear = year - age;
 halfmoney = money / 2
-const OUTPUT = document.getelmentByID("spaceforJavaScriptOutput")
-OUTPUT.innerHTML += "<h2> "Added by JavaScript"</h2>
-OUTPUT.innerHTML += console.log("<p>You were born in " + birthyear</p>" )
-OUTPUT.innerHTML += console.log("<p>In 10 years you will be " + oldAge</p>")
-OUTPUT.innerHTML += console.log("<p>You have $" + money</p>")
-OUTPUT.innerHTML += console.log("<p>Uh-oh! You spent half of your money! You now have $" + halfmoney</p>")
+const OUTPUT = document.getelmentByID("spaceforJavaScriptOutput");
+OUTPUT.innerHTML += "<h2> "Added by JavaScript"</h2>";
+OUTPUT.innerHTML += "<p>You were born in " + birthyear</p>";
+OUTPUT.innerHTML += "<p>In 10 years you will be " + oldAge</p>";
+OUTPUT.innerHTML += "<p>You have $" + money</p>");
+OUTPUT.innerHTML += "<p>Uh-oh! You spent half of your money! You now have $" + halfmoney</p>";
 
 
 
