891fce5e8f650446a0bb31d02c61607f305eb564
cole-g-123 <24316cg@hvhs.school.nz>
2026-05-03 22:51:12 +0000

    May 3, 2026, 10:51 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -14,7 +14,7 @@ halfmoney = money / 2
 const OUTPUT = document.getelmentByID("spaceforJavaScriptOutput")
 OUTPUT.innerHTML += "<h2> "Added by JavaScript"</h2>
 OUTPUT.innerHTML += console.log("<p>You were born in " + birthyear</p>" )
-OUTPUT.innerHTML += console.log("<p>In 10 years you will be " + oldge</p>")
+OUTPUT.innerHTML += console.log("<p>In 10 years you will be " + oldAge</p>")
 OUTPUT.innerHTML += console.log("<p>You have $" + money</p>")
 OUTPUT.innerHTML += console.log("<p>Uh-oh! You spent half of your money! You now have $" + halfmoney</p>")
 
