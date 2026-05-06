506d78a7d4d697a5ed2771bf05e7967f3e1045f7
cole-g-123 <24316cg@hvhs.school.nz>
2026-05-06 02:38:16 +0000

    May 6, 2026, 2:38 AM

--- t06_functions.js
+++ t06_functions.js
@@ -1,1 +1,29 @@
-/* Paste your code from task 5 here */
+/
+console.log( "Running t.05_javascriptandhtml.js");
+
+//Variables
+let username = "Cole";
+let year = 2026;
+let age = 15;
+let money = 378;
+let birthyear;
+let oldage;
+oldAge = age + 10;
+birthyear = year - age;
+halfmoney = money / 2
+const OUTPUT = document.getelmentByID("spaceforJavaScriptOutput");
+OUTPUT.innerHTML += "<h2> "Added by JavaScript" + </h2>";
+OUTPUT.innerHTML += "<p>You were born in " + birthyear + </p>";
+OUTPUT.innerHTML += "<p>In 10 years you will be " + oldAge + </p>";
+OUTPUT.innerHTML += "<p>You have $" + money + </p>");
+OUTPUT.innerHTML += "<p>Uh-oh! You spent half of your money! You now have $" + halfmoney + </p>";
++
+
+
+
+
+
+/*******************
+ *Functions
+ *******************/
+
