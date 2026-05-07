90bec518b7924a61f2fa4b9bae5c8ddc0c8098fb
cole-g-123 <24316cg@hvhs.school.nz>
2026-05-07 21:34:52 +0000

    May 7, 2026, 9:34 PM

--- t09_activate_via_button.js
+++ t09_activate_via_button.js
@@ -1,1 +1,67 @@
-/* Paste your code from task 7 here*/
+
+console.log( "Running t01_introduction.js" );
+
+//Variables
+let username = "Cole";
+let year = 2026;
+let age = 15;
+let money = 378;
+let birthyear;
+let oldage;
+let userName = "Ms Alice,"
+oldage = age + 10;
+birthyear = year - age;
+halfmoney = money / 2
+<button onClick="start()">START</button>
+
+console.log("You were born in " + birthyear )
+console.log("In 10 years you will be " + oldage)
+console.log("You have $" + money)
+console.log("Uh-oh! You spent half of your money! You now have $" + halfmoney)
+
+const OUTPUT = document.getElementById("JavaScriptOutput");
+
+OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>";
+OUTPUT.innerHTML += "<p>Hello " + username + "</p>";
+OUTPUT.innerHTML += "<p>Second Paragraph line.</p>";
+OUTPUT.innerHTML += "<p>Third Paragraph line.</p>";
+
+
+displayWelcome(userName, 16);
+
+
+
+/*******************
+ *Functions
+ *******************/
+function writeLine(){
+   // Add a line to the html page
+   OUTPUT.innerhtml += "<p>Less code matters</p>";
+}
+
+
+/****************************
+  Main Code
+****************************/ 
+writeLine()
+
+
+
+
+
+/*******************
+ *Functions
+ *******************/
+function writeLine(){
+   // Add a line to the html page
+   OUTPUT.innerhtml += "<p>Less code matters</p>";
+}
+ 
+function displayWelcome(_name, _age){
+    OUTPUT.innerHTML += "<p>Welcome to the page "+_name+"</p>";
+    OUTPUT.innerHTML += "<p>You are "+_age+" years old</p>";
+}
+/****************************
+  Main Code
+****************************/ 
+writeLine()
