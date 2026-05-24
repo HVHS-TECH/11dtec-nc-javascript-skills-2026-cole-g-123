b1a5f9c0a0449103805f11b75aec6c93d0b535e7
cole-g-123 <24316cg@hvhs.school.nz>
2026-05-24 22:45:33 +0000

    May 24, 2026, 10:45 PM

--- t12_conditionals.js
+++ t12_conditionals.js
@@ -1,17 +1,67 @@
-/*
-  t12_conditionals.js
-  Checks whether the user has enough pocket money to buy a $4 chocolate bar.
-*/
 
-console.log("Running t12_conditionals.js");
+console.log( "Running t01_introduction.js" );
 
-let pocketMoney = 3; // change this value to test affordability
-let chocolatePrice = 4;
+//Variables
+let username = "Cole";
+let year = 2026;
+let age = 15;
+let money = 378;
+let birthyear;
+let oldage;
+let userName = "Ms Alice,"
+oldage = age + 10;
+birthyear = year - age;
+halfmoney = money / 2
+currentYear = "2026"
 
-console.log("A chocolate bar costs $" + chocolatePrice);
 
-if (pocketMoney >= chocolatePrice) {
-  console.log("\nYou CAN afford a chocolate bar");
-} else {
-  console.log("\nSorry you CAN'T afford a chocolate bar");
+
+
+const OUTPUT = document.getElementById("JavaScriptOutput");
+
+
+
+
+
+
+
+function start() {
+    spaceForJavaScriptOutput.innerHTML
+    spaceForJavaScriptOutput.innerHTML
+    answer = currentYear - userAge;
+    spaceForJavaScriptOutput.innerHTML
 }
+
+
+
+
+
+ 
+function displayWelcome(_name, _age){
+    OUTPUT.innerHTML += "<p>Welcome to the page "+_name+"</p>";
+    OUTPUT.innerHTML += "<p>You are "+_age+" years old</p>";
+}
+/****************************
+  Main Code
+****************************/ 
+function start() {
+    spaceForJavaScriptOutput.innerHTML
+    spaceForJavaScriptOutput.innerHTML
+    answer = currentYear - userAge;
+    spaceForJavaScriptOutput.innerHTML
+}
+
+if (age < 15){
+    console.log("Definitely too young")
+}
+
+if (age >= 15){
+    console.log("Might be too old")
+}
+
+if (age = 15){
+    console.log("Just right")
+}
+
+
+
--- t13_functions_return.js
+++ t13_functions_return.js
@@ -1,1 +1,67 @@
-/* Paste your code from the last task */
+
+console.log( "Running t01_introduction.js" );
+
+//Variables
+let username = "Cole";
+let year = 2026;
+let age = 15;
+let money = 378;
+let birthyear;
+let oldage;
+let userName = "Ms Alice,"
+oldage = age + 10;
+birthyear = year - age;
+halfmoney = money / 2
+currentYear = "2026"
+
+
+
+
+const OUTPUT = document.getElementById("JavaScriptOutput");
+
+
+
+
+
+
+
+function start() {
+    spaceForJavaScriptOutput.innerHTML
+    spaceForJavaScriptOutput.innerHTML
+    answer = currentYear - userAge;
+    spaceForJavaScriptOutput.innerHTML
+}
+
+
+
+
+
+ 
+function displayWelcome(_name, _age){
+    OUTPUT.innerHTML += "<p>Welcome to the page "+_name+"</p>";
+    OUTPUT.innerHTML += "<p>You are "+_age+" years old</p>";
+}
+/****************************
+  Main Code
+****************************/ 
+function start() {
+    spaceForJavaScriptOutput.innerHTML
+    spaceForJavaScriptOutput.innerHTML
+    answer = currentYear - userAge;
+    spaceForJavaScriptOutput.innerHTML
+}
+
+if (age < 15){
+    console.log("Definitely too young")
+}
+
+if (age >= 15){
+    console.log("Might be too old")
+}
+
+if (age = 15){
+    console.log("Just right")
+}
+
+
+
