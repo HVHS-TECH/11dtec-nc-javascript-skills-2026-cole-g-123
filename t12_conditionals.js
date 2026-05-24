db4291717c8d8be7709b40fc8aa61d19aa46dc63
cole-g-123 <24316cg@hvhs.school.nz>
2026-05-24 22:25:10 +0000

    May 24, 2026, 10:25 PM

--- t12_conditionals.js
+++ t12_conditionals.js
@@ -1,1 +1,88 @@
-/* Paste your code from the last task */
+
+console.log( "Running t01_introduction.js" );
+
+//Variables
+let username = "Cole";
+let year = 2026;
+let age = 15;
+let money = 378;
+let birthyear;
+let oldage;
+let userName = "Ms Alice,"
+oldage = age + 10;
+birthyear = year - age;
+halfmoney = money / 2
+currentYear = "2026"
+
+
+console.log("You were born in " + birthyear )
+console.log("In 10 years you will be " + oldage)
+console.log("You have $" + money)
+console.log("Uh-oh! You spent half of your money! You now have $" + halfmoney)
+
+const OUTPUT = document.getElementById("JavaScriptOutput");
+
+OUTPUT.innerHTML += "<h2>Added by JavaScript</h2>";
+OUTPUT.innerHTML += "<p>Hello " + username + "</p>";
+OUTPUT.innerHTML += "<p>Second Paragraph line.</p>";
+OUTPUT.innerHTML += "<p>Third Paragraph line.</p>";
+
+
+displayWelcome(userName, 16);
+
+
+
+/*******************
+ *Functions
+ *******************/
+function writeLine(){
+   // Add a line to the html page
+   OUTPUT.innerhtml += "<p>Less code matters</p>";
+}
+
+function start() {
+    spaceForJavaScriptOutput.innerHTML
+    spaceForJavaScriptOutput.innerHTML
+    answer = currentYear - userAge;
+    spaceForJavaScriptOutput.innerHTML
+}
+
+/****************************
+  Main Code
+****************************/ 
+writeLine()
+
+var num1 = "50";
+num1+num1;
+
+var num2 = 50;
+num2+num2;
+
+num1 = Number(num1);
+num1+num1;
+
+let userAgeString = AGE_FIELD.value;
+let userAgeNumber = Number(userAgeString)
+
+
+/*******************
+ *Functions
+ *******************/
+function writeLine(){
+   // Add a line to the html page
+   OUTPUT.innerhtml += "<p>Less code matters</p>";
+}
+ 
+function displayWelcome(_name, _age){
+    OUTPUT.innerHTML += "<p>Welcome to the page "+_name+"</p>";
+    OUTPUT.innerHTML += "<p>You are "+_age+" years old</p>";
+}
+/****************************
+  Main Code
+****************************/ 
+if (age < 15){
+    console.log("Definitely too young")
+}
+writeLine()
+
+
