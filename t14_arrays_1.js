4f3a98cfa225836af7f83ea81c3eb5dca8e4417b
cole-g-123 <24316cg@hvhs.school.nz>
2026-05-25 23:52:56 +0000

    May 25, 2026, 11:52 PM

--- t14_arrays_1.js
+++ t14_arrays_1.js
@@ -1,1 +1,54 @@
-/* Paste your code from the last task */
+
+console.log( "Running t01_introduction.js" );
+
+//Variables
+let username = "Cole";
+let year = 2026;
+let age = 15;
+let money = 378;
+let birthyear;
+let oldage;
+let userName = "Ms Alice,"
+oldage = age + 10;
+birthyear = year - age;
+halfmoney = money / 2
+currentYear = "2026"
+
+
+
+
+const OUTPUT = document.getElementById("JavaScriptOutput");
+
+
+function start() {
+    spaceForJavaScriptOutput.innerHTML
+    spaceForJavaScriptOutput.innerHTML
+    answer = currentYear - userAge;
+    spaceForJavaScriptOutput.innerHTML
+}
+
+ 
+function displayWelcome(_name, _age){
+    OUTPUT.innerHTML += "<p>Welcome to the page "+_name+"</p>";
+    OUTPUT.innerHTML += "<p>You are "+_age+" years old</p>";
+}
+/****************************
+  Main Code
+****************************/ 
+
+
+
+function calculatePay(hours){
+    let pay = _hours * 18.80; // Training wage
+    pay = pay - (pay * 0.105); // Tax deduction
+     pay = pay - (pay * 0.04); // Kiwisaver
+     pay = pay + 10; // bonus
+        return pay;
+}
+
+let MondayPay = calculatePay(8);
+let TuesdayPay = calculatePay(6);
+
+`OUTPUT.innerHTML =+ "This week's pay:<br>"
+OUTPUT.innerHTML += "Monday: $" +MondayPay+"<br>";
+OUTPUT.innerHTML += "Tuesday: $" +TuesdayPay+"<br>";`
