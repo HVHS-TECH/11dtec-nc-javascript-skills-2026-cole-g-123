392b1f85d94d1709b36a73dd63e818d38b76c559
cole-g-123 <24316cg@hvhs.school.nz>
2026-05-26 00:25:01 +0000

    May 26, 2026, 12:25 AM

--- t14_arrays_1.js
+++ t14_arrays_1.js
@@ -48,4 +48,4 @@ OUTPUT.innerHTML += "<br>The fourth class is " + classArray[4];
 
 let classArray = ["Maths", "English", "Science", "History", "PE"];
 let choice = CHOOSE_FIELD.value
-OUTPUT.innerHTML = "The class you chose is "+classArray[choice]+"<br>"
+OUTPUT.innerHTML = "The class you chose is "+classArray[choice];
